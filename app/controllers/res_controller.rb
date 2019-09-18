167da25c0c3b72f80b9fb8f63788966cbe11ba9f
dvik1950 <despic.viktor@gmail.com>
2019-09-18 10:06:55 +0200

    Solve issue by fixing routes.rb

--- app/controllers/res_controller.rb
+++ app/controllers/res_controller.rb
@@ -8,16 +8,19 @@ class ResController < ApplicationController
   def a
     @re = Re.find(params[:id])
     @re.update_attribute(:some_attribute, "A")
+    redirect_to root_path
   end
 
   def b
     @re = Re.find(params[:id])
     @re.update_attribute(:some_attribute, "B")
+    redirect_to root_path
   end
 
   def c
     @re = Re.find(params[:id])
     @re.update_attribute(:some_attribute, "C")
+    redirect_to root_path
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'res#index'
-  put 'res/a', to: 'res#a', as: 'a'
-  put 'res/b', to: 'res#b', as: 'b'
-  put 'res/c', to: 'res#c', as: 'c'
+  put 'res/a/:id', to: 'res#a', as: 'a'
+  put 'res/b/:id', to: 'res#b', as: 'b'
+  put 'res/c/:id', to: 'res#c', as: 'c'
 end
